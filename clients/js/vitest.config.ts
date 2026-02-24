7cf2cc3be24b8e92c8bd0170ddd518998aec4ced
Loris Leiva <loris.leiva@gmail.com>
2026-02-24 20:58:05 +0500

    Bump JS renderer (#25)

--- clients/js/vitest.config.ts
+++ clients/js/vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         __VERSION__: `"${env.npm_package_version}"`,
     },
     test: {
-        // ...
+        environment: 'node',
+        testTimeout: 15000,
     },
 });
